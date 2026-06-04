0868d227594fb0bc47a146ad3c5e77c0ff038cc9
ratheesh2804 <ratheeshuday28@gmail.com>
2026-06-04 10:02:48 +0000

    Added luxury LUXORA UI

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function shopNow(){
-    alert("Welcome to NovaCart!");
+function explore() {
+    alert("Welcome to LUXORA Premium Collection");
 }
+
+window.addEventListener("scroll", () => {
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        const position = card.getBoundingClientRect().top;
+
+        if(position < window.innerHeight - 100){
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+
+    });
+
+});
